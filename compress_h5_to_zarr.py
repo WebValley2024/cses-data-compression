5835eaca8d54bf6880ba000629a74e9436b1b160
Luca Coviello <luca.coviello@gmail.com>
2024-06-26 12:25:18 +0200

    Add first version of compression scripts

---
+++ compress_h5_to_zarr.py
@@ -0,0 +1,60 @@
+import os
+from glob import glob
+from multiprocessing import Pool
+from functools import partial
+
+import fire
+import numpy as np
+import xarray as xr
+import zarr
+from numcodecs import Blosc, blosc
+from tqdm import tqdm
+
+
+def worker(file, outfolder=None):
+    try:
+        zarrfile = file.replace(".h5", ".zarr.zip")
+
+        if outfolder is not None:
+            os.makedirs(outfolder, exist_ok=True)
+            # keep only the filename
+            zarrfile = os.path.basename(zarrfile)
+            # add the output folder
+            zarrfile = os.path.join(outfolder, zarrfile)
+
+        compressor = Blosc(cname="zstd", clevel=9, shuffle=Blosc.AUTOSHUFFLE)
+        ds = xr.open_dataset(file)
+
+        time_dim = ds.UTC_TIME.dims[0]
+        encodings = {}
+
+        for k in ds.data_vars:
+            if ds[k].dims[0] == time_dim:
+                # use chunks of 128 along the time dimension
+                chunks = list(ds[k].shape)
+                chunks[0] = 128 if chunks[0] > 128 else chunks[0]
+                encodings[k] = {"compressor": compressor, "chunks": tuple(chunks)}
+            else:
+                # do not chunk along other dimensions
+                encodings[k] = {"compressor": compressor}
+
+        store = ds.to_zarr(zarrfile, mode="w", encoding=encodings)
+        store.close()
+
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
+
+
+def main(data_dir: str, outfolder: str, n_workers: int = 8):
+    files = glob(os.path.join(data_dir, "**/*.h5"), recursive=True)
+    pool = Pool(n_workers)
+
+    for _ in tqdm(
+        pool.imap_unordered(partial(worker, outfolder=outfolder), files),
+        total=len(files),
+    ):
+        pass
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ run_compression.sh
@@ -0,0 +1,7 @@
+# python compress_h5_to_zarr.py /disks/wd3/LAP /disks/wv24/LAP/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/EFD_ULF /disks/wv24/EFD_ULF/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/HEPD /disks/wv24/HEPD/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/HEPP_H /disks/wv24/HEPP_H/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/HEPP_L /disks/wv24/HEPP_L/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/HEPP_X /disks/wv24/HEPP_X/ -n 32
+python compress_h5_to_zarr.py /disks/wd3/SCM_ULF /disks/wv24/SCM_ULF/ -n 32
